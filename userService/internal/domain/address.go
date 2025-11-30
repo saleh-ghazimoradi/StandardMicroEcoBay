3a7216edcf6b98b5d46abdc74781769af1116404
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-11-30 19:30:51 +0330

    add user and address domains to the porject

---
+++ userService/internal/domain/address.go
@@ -0,0 +1,13 @@
+package domain
+
+type Address struct {
+	Id           int64  `json:"id"`
+	AddressLine1 string `json:"address_line1"`
+	AddressLine2 string `json:"address_line2"`
+	City         string `json:"city"`
+	PostCode     string `json:"post_code"`
+	Country      string `json:"country"`
+	UserId       int64  `json:"user_id"`
+	CreatedAt    int64  `json:"created_at"`
+	UpdatedAt    int64  `json:"updated_at"`
+}
---
+++ userService/internal/domain/user.go
@@ -0,0 +1,16 @@
+package domain
+
+import "time"
+
+type User struct {
+	Id         int64     `json:"id"`
+	Email      string    `json:"email"`
+	Password   string    `json:"-"`
+	FirstName  string    `json:"first_name"`
+	LastName   string    `json:"last_name"`
+	Phone      string    `json:"phone"`
+	ResetToken string    `json:"reset_token"`
+	Address    Address   `json:"address"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
