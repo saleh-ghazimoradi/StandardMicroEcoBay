fde277fa53280f44344653c65e54fbba7a25ce98
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-12-06 22:09:28 +0330

    fix the issue of created_at and updated_at

--- productService/internal/domain/product.go
+++ productService/internal/domain/product.go
@@ -1,15 +1,17 @@
 package domain
 
+import "time"
+
 type Product struct {
-	Id          int64   `json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	CategoryId  int64   `json:"category_id"`
-	Price       float64 `json:"price"`
-	Stock       uint    `json:"stock"`
-	ImageURL    string  `json:"image_url"`
-	Status      string  `json:"status"`
-	CreatedAt   int64   `json:"created_at"`
-	UpdatedAt   int64   `json:"updated_at"`
-	Version     int     `json:"version"`
+	Id          int64     `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	CategoryId  int64     `json:"category_id"`
+	Price       float64   `json:"price"`
+	Stock       uint      `json:"stock"`
+	ImageURL    string    `json:"image_url"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Version     int       `json:"version"`
 }
