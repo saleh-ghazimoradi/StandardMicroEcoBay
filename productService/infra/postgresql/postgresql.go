ccce5597f09c1737a95f37919216fd9820c6971d
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-12-06 15:35:21 +0330

    establish postgresql DB connection

---
+++ productService/infra/postgresql/postgresql.go
@@ -0,0 +1,116 @@
+package postgresql
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+type Postgresql struct {
+	Host        string
+	Port        string
+	User        string
+	Password    string
+	Name        string
+	MaxOpenConn int
+	MaxIdleConn int
+	MaxIdleTime time.Duration
+	SSLMode     string
+	Timeout     time.Duration
+}
+
+type Options func(*Postgresql)
+
+func WithHost(host string) Options {
+	return func(p *Postgresql) {
+		p.Host = host
+	}
+}
+
+func WithPort(port string) Options {
+	return func(p *Postgresql) {
+		p.Port = port
+	}
+}
+
+func WithUser(user string) Options {
+	return func(p *Postgresql) {
+		p.User = user
+	}
+}
+
+func WithPassword(password string) Options {
+	return func(p *Postgresql) {
+		p.Password = password
+	}
+}
+
+func WithName(name string) Options {
+	return func(p *Postgresql) {
+		p.Name = name
+	}
+}
+
+func WithMaxOpenConn(maxConn int) Options {
+	return func(p *Postgresql) {
+		p.MaxOpenConn = maxConn
+	}
+}
+
+func WithMaxIdleConn(maxIdleConn int) Options {
+	return func(p *Postgresql) {
+		p.MaxIdleConn = maxIdleConn
+	}
+}
+
+func WithMaxIdleTime(maxIdleTime time.Duration) Options {
+	return func(p *Postgresql) {
+		p.MaxIdleTime = maxIdleTime
+	}
+}
+
+func WithSSLMode(mode string) Options {
+	return func(p *Postgresql) {
+		p.SSLMode = mode
+	}
+}
+
+func WithTimeout(timeout time.Duration) Options {
+	return func(p *Postgresql) {
+		p.Timeout = timeout
+	}
+}
+
+func (p *Postgresql) uri() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", p.Host, p.Port, p.User, p.Password, p.Name, p.SSLMode)
+}
+
+func (p *Postgresql) Connect() (*sql.DB, error) {
+	db, err := sql.Open("postgres", p.uri())
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(p.MaxOpenConn)
+	db.SetMaxIdleConns(p.MaxIdleConn)
+	db.SetConnMaxIdleTime(p.MaxIdleTime)
+
+	ctx, cancel := context.WithTimeout(context.Background(), p.Timeout)
+	defer cancel()
+
+	if err = db.PingContext(ctx); err != nil {
+		if err = db.Close(); err != nil {
+			return nil, err
+		}
+	}
+	return db, nil
+}
+
+func NewPostgresql(opts ...Options) *Postgresql {
+	p := &Postgresql{}
+	for _, o := range opts {
+		o(p)
+	}
+	return p
+}
