8486e4a716c234c688531d681683080ab7eecca8
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-12-06 15:39:47 +0330

    set JWT config

--- userService/config/application.go
+++ userService/config/application.go
@@ -1,6 +1,10 @@
 package config
 
+import "time"
+
 type Application struct {
-	Version     string `env:"VERSION"`
-	Environment string `env:"ENVIRONMENT"`
+	Version     string        `env:"VERSION"`
+	Environment string        `env:"ENVIRONMENT"`
+	JWTSecret   string        `env:"JWT_SECRET"`
+	JWTExp      time.Duration `env:"JWT_EXPIRES"`
 }
