c5071c72e6c1ee4a183209705799e19040a7d344
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-12-06 15:00:31 +0330

    rewrite user and address scripts

--- userService/infra/migrations/0001_create_users_table.up.sql
+++ userService/infra/migrations/0001_create_users_table.up.sql
@@ -1,13 +1,13 @@
 CREATE EXTENSION IF NOT EXISTS citext;
 CREATE TABLE IF NOT EXISTS users(
-    id bigserial PRIMARY KEY,
-    email citext UNIQUE NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    email CITEXT UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     phone VARCHAR(50),
     reset_token VARCHAR(255),
-    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
     version INTEGER NOT NULL DEFAULT 1
 );
